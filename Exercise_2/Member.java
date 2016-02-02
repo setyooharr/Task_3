1cf3a8129957146619f60efd5a8f87afa77b9d93
Setyo Haryo A <setyoharyo37@gmail.com>
2016-02-02 22:06:18 +0700

    Update Member.java

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,43 @@
+public class Member {
+	private String name;
+	private String specialization;
+	private int projectWorked = 0;
+	
+	public Member(String name) {
+		this.name = name;
+	}
+	
+	public Member(String name, String specialization) {
+		this.name = name;
+		this.specialization = specialization;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSpecialization() {
+		return specialization;
+	}
+	
+	public int setProjectWorked() {
+		return projectWorked;
+	}
+	
+	public void getProjectWorked(int projectWorked) {
+		this.projectWorked = projectWorked;
+	}
+	
+	public String toString() {
+		return "Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" projec(s)";
+	}
+}
 
