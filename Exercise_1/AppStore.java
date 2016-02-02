b2b4fbf85beaa5bf6f402c9229c73da252c7479a
Setyo Haryo A <setyoharyo37@gmail.com>
2016-02-02 21:57:34 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,26 @@
+public class AppStore {
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
+	
+	public createNewApp(String appName, int appSize) {
+		appList[totalApp] = new Application(appName,appSize);
+		totalApp++;
+	}
+	
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp;
+	}
+	
+	public Application[] getAppList() {
+		return appList;
+	}
+	
+	public String toString() {
+		return "There are "+totalApp+" application ready to install";
+	}
+}
 
