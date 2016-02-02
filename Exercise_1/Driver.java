6e67001e580c4539a1a3c989a9f8a68fb18571c4
Setyo Haryo A <setyoharyo37@gmail.com>
2016-02-02 21:59:25 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,19 @@
+public class Driver {
+	public static void main(String[] args) {
+		AppStore aps = new Appstore();
+		SmartPhone sm = new SmartPhone();
+		aps.createNewApp("app1",100);
+		aps.createNewApp("app2",200);
+		aps.createNewApp("app3",300);
+		aps.createNewApp("app4",400);
+		System.out.println(aps.toString());
+		System.out.println(aps.getApp(2).toString());
+		sm.setMemory(300);
+		System.out.println(sm.toString());
+		sm.addApplication(aps,app1);
+		System.out.println(sm.toString());
+		sm.addApplication(aps,app2);
+		System.out.println(sm.toString());
+	}
+}
 
