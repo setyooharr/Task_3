5a5a7e0219123a2cfdea4f907994bb6ac16c9b8b
Setyo Haryo A <setyoharyo37@gmail.com>
2016-02-02 22:09:30 +0700

    Update StartUp.java

--- Exercise_2/StartUp.java
+++ Exercise_2/StartUp.java
@@ -1,1 +1,50 @@
+public class StartUp {
+	private Member[] member = new Member[50];
+	private Project[] projectList = new Project[50];
+	private int nMember;
+	private int nProject;
+	private int x = 0;
+	private int y = 0;
+	private int z = 0;
+	
+	public void addMember(Member m) {
+		member[nMember] = m;
+		nMember++;
+	}
+	
+	public Member getMember(int id) {
+		return member[id];
+	}
+	
+	public String createNewProject(String projectName) {
+		projectList[nProject] = new projectName[projectList];
+		nProject++;
+	}
+	
+	public Project getProject(int id) {
+		return projectList[id];
+	}
+	
+	public void setProjectMember(Project p, Member m) {
+		x=m.getProjectWorked();
+		x++;
+		p.addMember(m);
+		m.setProjectMember(a);
+	}
+	
+	public void releaseProject(Project p) {
+		p.releaseApp();
+	}
+	
+	public int getNumReleasedProject() {
+		while(b<projectList.length) {
+			if(projectList[y].isReleased()==true) {
+				z=y+1;
+				y=projectList.length;
+			}
+			y++;
+		}
+	return z;
+	}
+}
 
