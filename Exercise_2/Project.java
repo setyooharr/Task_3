dc2bbe818f5acc7ac60b431161de31ea5c8dc454
Setyo Haryo A <setyoharyo37@gmail.com>
2016-02-02 22:07:28 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,35 @@
+public class Project {
+	private String projectName;
+	private Member[] teamMember = new Member[5];
+	private int nTeam = 0;
+	private boolean releaseStatus = false;
+	
+	public Project(String projectName){
+		this.projectName = projectName;
+		teamMember = new Member[5];
+		releaseStatus = false;
+	}
+	
+	public void addMember(Member m) {
+		teamMember[nTeam] = m;
+		nTeam++;
+	}
+	
+	public boolean isReleased() {
+		return releaseStatus;
+	}
+	
+	public void releaseApp() {
+		releaseStatus = true;
+	}
+	
+	public String toString() {
+		if(releaseStatus==true) {
+			return "Project "+projectName+" status is Released with team member of "+nTeam;
+		}
+		else(releaseStatus==false) {
+			return "Project "+projectName+" status is in progress with team member of "+nTeam;
+		}
+	}
+}
 
