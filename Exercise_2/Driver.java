05efca246f09989a0e6b224760be469cf7b74021
Setyo Haryo A <setyoharyo37@gmail.com>
2016-02-02 22:05:44 +0700

    Update Driver.java

--- Exercise_2/Driver.java
+++ Exercise_2/Driver.java
@@ -1,29 +1,29 @@
 public class Driver {
 	public static void main(String[] args) {
 		StartUp stp = new StartUp();
-		Member m = new Member("Alex");
-		Member m = new Member("Beni);
-		Member m = new Member("Cakra");
-		Member m = new Member("Deni");
-		Member m = new Member("Eric",");
-		stp.addMember(m);
-		stp.addMember(m);
-		stp.addMember(m);
-		stp.addMember(m);
-		stp.addMember(m);
+		Member x = new Member("Alex");
+		Member x = new Member("Beni);
+		Member x = new Member("Cakra");
+		Member x = new Member("Deni");
+		Member x = new Member("Eric",");
+		stp.addMember(x);
+		stp.addMember(x);
+		stp.addMember(x);
+		stp.addMember(x);
+		stp.addMember(x);
 		
 		stp.createNewProject("project 1");
 		stp.createNewProject("project 2");
 		
 		Project p = stp.getProject(0);
-		m = stp.getProject(0);
-		stp.setProjectMember(p,m);
+		x = stp.getProject(0);
+		stp.setProjectMember(p,x);
 		p = stp.getProject(1);
-		stp.setProjectMember(p,m);
+		stp.setProjectMember(p,x);
 		
 		m = stp.getMember(4);
 		p = stp.getProject(0);
-		stp.setProjectMember(p,m);
+		stp.setProjectMember(p,x);
 		
 		stp.releaseProject(stp.getProject(0));
 		
